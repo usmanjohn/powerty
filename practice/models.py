dc50b4c0fef060ceeb03fb6a9bd516edf6c67b72
PivotAlfa <backpacker0220@gmail.com>
2024-10-19 14:54:23 +0900

    db change

--- practice/models.py
+++ practice/models.py
@@ -10,8 +10,16 @@ class Practice(models.Model):
     ('verbal', 'Verbal'),
     ('insight','Insight'),
     ('all','All')
-)
+    )
+    Easy_level = (
+        ('easy','Easy'),
+        ('Medium', 'Medium'),
+        ('hard','Hard'),
+        ('mixed','Mixed'),
+        ('real', 'Real')
+    )
     category = models.CharField(choices=Category_CHOICES, default='quant', max_length=20)
+    level = models.CharField(choices=Easy_level, default='real', max_length=20)
 
     title = models.CharField(max_length=200)
     description = models.TextField()
@@ -30,14 +38,15 @@ class PracitceQuestions(models.Model):
     practice = models.ForeignKey(Practice, on_delete=models.CASCADE, related_name='practice_questions')
     question_text = CKEditor5Field('Question', config_name='extends')
     image = models.ImageField(storage=S3Boto3Storage(),upload_to='pracitce_images/', null=True, blank=True)
-    choice1 = models.CharField(max_length=200)
-    choice2 = models.CharField(max_length=200)
-    choice3 = models.CharField(max_length=200)
-    choice4 = models.CharField(max_length=200)
-    choice5 = models.CharField(max_length=200)
-    category = models.CharField(choices=Category_CHOICES, default='quant', max_length=20)
+    choice1 = CKEditor5Field('Choice 1', config_name='extends')
+    choice2 = CKEditor5Field('Choice 2', config_name='extends')
+    choice3 = CKEditor5Field('Choice 3', config_name='extends')
+    choice4 = CKEditor5Field('Choice 4', config_name='extends')
+    choice5 = CKEditor5Field('Choice 5', config_name='extends')
+    
     correct_answer = models.IntegerField(choices=[(1, 'Choice 1'), (2, 'Choice 2'), (3, 'Choice 3'), (4, 'Choice 4'), (5, 'Choice 5')])
-    explanation = models.TextField(null=True, blank=True)
+    explanation = CKEditor5Field('Explanation', config_name='extends', null=True, blank=True)
+    category = models.CharField(choices=Category_CHOICES, default='quant', max_length=20)
     made_by = models.ForeignKey(User, related_name='practice_questions', on_delete=models.CASCADE)
     date_made = models.DateTimeField(auto_now_add=True)
     link = models.URLField(blank=True, null=True)
